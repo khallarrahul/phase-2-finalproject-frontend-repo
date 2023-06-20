fb8d9b1cb550d4bd83ae438f19be67f7ab4a0eab
RAHUL KHALLAR <khallarrahul@gmail.com>
2023-06-19 18:24:06 -0700

    adds data to DOM

--- src/components/EventCard.js
+++ src/components/EventCard.js
@@ -2,8 +2,16 @@ import React from "react";
 import "./EventCard.css";
 
 function EventCard({ event }) {
-  console.log(event);
-  return <div>{event.name}</div>;
+  console.log(event.classifications);
+  //const genreMapper = event.classifications.genre.map((gen) => gen.name);
+  const genreName = event.classifications[0].genre.name;
+  return (
+    <div className="eventCard">
+      <h1>{event.name}</h1>
+      <h2>Type of Event: {genreName}</h2>
+      <h2>Get your tickets here: {event.url}</h2>
+    </div>
+  );
 }
 
 export default EventCard;

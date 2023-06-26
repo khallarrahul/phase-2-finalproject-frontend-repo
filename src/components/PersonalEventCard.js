2186120a6a1ad3ca2ca3711394583578c6609c96
RAHUL KHALLAR <khallarrahul@gmail.com>
2023-06-26 09:00:58 -0700

    adds control if statement to patch request to stop attendees to go below 0

--- src/components/PersonalEventCard.js
+++ src/components/PersonalEventCard.js
@@ -19,17 +19,19 @@ function PersonalEventCard({ personalEvent, deletePersonalEvent }) {
   };
 
   const subtractAttendee = () => {
-    fetch(`https://json-server-o66d.onrender.com/personalEvents/${personalEvent.id}`, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        attendee: attendee - 1,
-      }),
-    })
-      .then((res) => res.json())
-      .then((data) => setAttendee(parseInt(data.attendee)));
+    if (attendee > 0) {
+      fetch(`https://json-server-o66d.onrender.com/personalEvents/${personalEvent.id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          attendee: attendee - 1,
+        }),
+      })
+        .then((res) => res.json())
+        .then((data) => setAttendee(parseInt(data.attendee)));
+    }
   };
 
   const deleteCard = () => {

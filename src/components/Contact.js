96e3fa9e736e76e9ce91d5f5f94d8ef6a051abb3
RAHUL KHALLAR <khallarrahul@gmail.com>
2023-06-26 17:50:34 -0700

    adds css to contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,8 +4,8 @@ import "./Contact.css";
 function Contact() {
   return (
     <div className="contact">
-      <p>This Project is a part of Software Development cohort at Flatiron School made by: Rahul Khallar</p>
-      <p>For furthur suggestions or improvements, contact: khallarrahul@gmail.com</p>
+      <h1>This Project is a part of Software Development cohort at Flatiron School made by: Rahul Khallar</h1>
+      <h1>For furthur suggestions or improvements, contact: khallarrahul@gmail.com</h1>
     </div>
   );
 }
